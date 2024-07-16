bbcd3323969c0ca884d0cde6754b74d0f025a175
linlin950315 <zlinlin969@gmail.com>
2024-07-16 10:13:58 +0900

    first commit

---
+++ library/src/main/java/com/example/library/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.example.library.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class BookController {
+    // @Autowired
+    // private BookService bookService;
+    /*
+     * 新增book
+     */
+    // @PostMapping("test2")
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test @RequestMapping(\"/test\")";
+    }
+
+}
---
+++ library/src/main/resources/templates/service/BookService.java
@@ -0,0 +1,5 @@
+package com.example.book_library.service;
+
+public class BookService {
+
+}
