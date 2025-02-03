30cedf615cb6e5a7ec727faeaa88c44af8c88ce9
zhangll <zhangll@tigersoft.co.jp>
2025-02-03 17:45:06 +0900

    CORS

---
+++ library/src/main/java/com/example/library/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 允许所有路径
+                        .allowedOrigins("http://localhost:4200") // 允许 Angular 前端访问
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+                        .allowedHeaders("*") // 允许所有请求头
+                        .allowCredentials(true); // 允许携带 Cookie
+            }
+        };
+    }
+}
